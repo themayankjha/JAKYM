ffae4d4037a1152ceda70e5b6ae5704d568726e2
Mayank Jha <jhamayank159@gmail.com>
2021-03-30 17:33:34 +0530

    Fixed playlist not working once whole queue is played

--- downloader.py
+++ downloader.py
@@ -6,7 +6,10 @@ from requests import get
 def download(link):
     options={
     'format': 'aac/mp3/ogg/wav/3gp/mp4',
-    'outtmpl': 'downloads/%(id)s',}
+    'outtmpl': 'downloads/%(id)s',
+    'quiet':'True'
+    }
+    print("Downloading")
     with YoutubeDL(options) as ytdl:
         try:
             get(link)
@@ -14,5 +17,6 @@ def download(link):
             meta = ytdl.extract_info(f"ytsearch:{link}", download=True)['entries'][0]
         else:
             meta = ytdl.extract_info(link, download=True)
+    print("Done Downloading")
     return meta
 
--- main.py
+++ main.py
@@ -1,35 +1,68 @@
 import downloader
 import player
-import os
+import os , glob
 import threading
+import pyfiglet
+
+class Playlist:
+
+    def __init__(self):
+        self.playlist=[]
+
+    def playlistmanager(self):
+        song=self.playlist.pop(0)
+        return song
+
+    def songqueuer(self,query):
+        self.playlist.append(query+" song")
+
+    def songdownloader(self,song):
+        meta=downloader.download(song)
+        return meta
+
+    def songplayer(self,meta):
+        print("Currently Playing : " + meta['title'])
+        player.playmusic('downloads/'+meta['id'])
+
+musicplaylist = Playlist()
+
+def cleandownload():
+    dir ='downloads'
+    for file in os.scandir(dir):
+        os.remove(file.path)
+
+def play():
+    while True:
+        if musicplaylist.playlist:
+            song=musicplaylist.playlistmanager()
+            meta=musicplaylist.songdownloader(song)
+            musicplaylist.songplayer(meta)
+            cleandownload()
+        else:
+            pass
+
+def queue():
+        songrequest=""
+        while songrequest!="exit":
+            songrequest=input("Enter your Song Name or Youtube Link: ")
+            musicplaylist.songqueuer(songrequest)
+            try:
+                playthread.start()
+            except:
+                pass
+
+queuethread=threading.Thread(target=queue)
+playthread=threading.Thread(target=play,daemon=True)
+
+f = pyfiglet.Figlet(font='slant')
+print(f.renderText('Console YouTube Music'))
+
+queuethread.start()
+queuethread.join()
+
+cleandownload()
 
 
-queue=[]
-
-def play(meta):
-    print("Currently Playing : " + meta['title'])
-    player.playmusic('downloads/'+meta['id'])
-    os.remove('downloads/'+meta['id']) 
-
-def getquery(queue):
-    query = input("Enter Song Name or Link: ")
-    return query+" song"
-
-def doqueue():
-    for query in queue:
-        meta=downloader.download(query)
-        play(meta)
-    
-
-t1 = threading.Thread(target=doqueue, args=(), daemon=True)
-
-while True:
-    query=getquery(queue)
-    queue.append(query)
-    try:
-        t1.start()
-    except:
-        pass
 
 
 
